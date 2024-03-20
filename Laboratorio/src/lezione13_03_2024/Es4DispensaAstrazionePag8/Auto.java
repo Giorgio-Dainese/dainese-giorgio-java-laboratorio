5a4390bbca9d8a56d95d5976843d06d4bc15e51d
Giorgio Dainese <giorgiodainese@A52-di-Giorgio.station>
2024-03-20 18:13:25 +0100

    Esercizi per il 21/03/2024

--- Laboratorio/src/lezione13_03_2024/Es4DispensaAstrazionePag8/Auto.java
+++ Laboratorio/src/lezione13_03_2024/Es4DispensaAstrazionePag8/Auto.java
@@ -1,4 +1,18 @@
 package lezione13_03_2024.Es4DispensaAstrazionePag8;
 
-public class Auto {
+public class Auto extends Vehicle {
+    public Auto(String modello) {
+        super(modello);
+    }
+
+    @Override
+    public void startEngine() {
+        String ret = "| Maranello NA V12 started     |";
+        System.out.println(ret);
+    }
+
+    @Override
+    public void stopEngine() {
+        System.out.println("| Maranello NA V12 shutted down     |");
+    }
 }
--- Laboratorio/src/lezione13_03_2024/Es4DispensaAstrazionePag8/Vehicle.java
+++ Laboratorio/src/lezione13_03_2024/Es4DispensaAstrazionePag8/Vehicle.java
@@ -1,6 +1,21 @@
 package lezione13_03_2024.Es4DispensaAstrazionePag8;
 
 public abstract class Vehicle {
+
+    public String modello;
+
+    public Vehicle(String modello) {
+        this.modello = modello;
+    }
+
+    public String getModello() {
+        return modello;
+    }
+
+    public void setModello(String modello) {
+        this.modello = modello;
+    }
+
     public abstract void startEngine();
     public abstract void stopEngine();
 }
